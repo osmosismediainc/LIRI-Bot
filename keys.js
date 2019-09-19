13d0a69b351e6a7187b80dff52abf21e0bfef764
osmosismediainc <osmosismediainc@gmail.com>
2019-09-19 19:41:24 -0400

    fix keys issue

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this KEYS is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
--- liri.js
+++ liri.js
@@ -1,3 +1,3 @@
 require("dotenv").config();
+var axios = require('axios');
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
